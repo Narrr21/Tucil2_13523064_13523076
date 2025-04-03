1743c078cc064a49ed5c82bde20f30fe95a53eda
Henshou <gerrardgrs@gmail.com>
2025-04-03 21:55:12 +0700

    Feat: image compression flow on main

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,13 @@ int main(int argc, char *argv[])
         cout << "Max depth reached: " << Blok::getMaxDepth() << endl;
         cout << "Block details:" << endl;
         blok->printBlok(0);
+        blok->normalizeRGB();
+        vector<vector<rgb>> outputMatrix(image.getHeight(), vector<rgb>(image.getWidth()));
+        blok->reconstructMatrix(outputMatrix);
+
+        ImageProcess outputImage;
+        outputImage.saveImage(handler->getOutputPath(), outputMatrix);
+
         cout << "Block details printed successfully." << endl;
     } else {
         cout << "Block is homogeneous or too small to divide." << endl;
