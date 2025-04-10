784142c2f6bd092f8316bcf3365596c69bde61e1
Henshou <gerrardgrs@gmail.com>
2025-04-10 20:23:37 +0700

    Refactor: improve file size read

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include "blok.hpp"
 #include "MAD.hpp"
 #include "ImageProcess.hpp"
-#include <chrono>
 #include "gif.h"
+#include <chrono>
 #include <cstdlib>
+#include <filesystem>
+#include <iomanip>
 
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -15,27 +18,27 @@ int main(int argc, char *argv[])
     auto start = chrono::high_resolution_clock::now();
 
     ImageProcess image(handler->getImagePath());
-
     vector<vector<rgb>> rgbMatrix = image.getRGBMatrix();
 
-    int originalSize = image.getWidth() * image.getHeight() * 3; 
-    cout << "Original image size (before compression): " << originalSize << " bytes" << endl;
-
-
+    error_code ec;
+    uintmax_t originalSize = filesystem::file_size(handler->getImagePath(), ec);
+    if (ec) {
+        cerr << "Error getting input file size: " << ec.message() << endl;
+    } else {
+        cout << fixed << setprecision(2);
+        cout << "Original image size: " << (originalSize / 1024.0) << " KB" << endl;
+    }
 
     Blok* blok = new Blok(0, 0, rgbMatrix, image.getWidth(), image.getHeight());
 
     if (blok->divide(handler->getmethod(), handler->getRange(), handler->getMinBlok())) {
-        int compressedSize = Blok::getCountBlok() * 3;
-        cout << "Compressed image size: " << compressedSize << " bytes" << endl;
         cout << "Total blocks created: " << Blok::getCountBlok() << endl;
         cout << "Max depth reached: " << Blok::getMaxDepth() << endl;
 
-        // cout << "Block details:" << endl;
-        // blok->printBlok(0);
         blok->normalizeRGB();
+
         GifWriter gifWriter;
-        GifBegin(&gifWriter, handler->getGifPath().c_str(), image.getWidth(), image.getHeight(), 100); 
+        GifBegin(&gifWriter, handler->getGifPath().c_str(), image.getWidth(), image.getHeight(), 100);
 
         for (int level = 0; level <= Blok::getMaxDepth(); ++level) {
             vector<vector<rgb>> frameMatrix(image.getHeight(), vector<rgb>(image.getWidth()));
@@ -48,13 +51,14 @@ int main(int argc, char *argv[])
                     buffer[idx++] = frameMatrix[y][x].getRed();
                     buffer[idx++] = frameMatrix[y][x].getGreen();
                     buffer[idx++] = frameMatrix[y][x].getBlue();
-                    buffer[idx++] = 255; 
+                    buffer[idx++] = 255;
                 }
             }
 
             GifWriteFrame(&gifWriter, buffer, image.getWidth(), image.getHeight(), 100);
             delete[] buffer;
         }
+
         GifEnd(&gifWriter);
 
         vector<vector<rgb>> outputMatrix(image.getHeight(), vector<rgb>(image.getWidth()));
@@ -62,16 +66,23 @@ int main(int argc, char *argv[])
 
         ImageProcess outputImage;
         outputImage.saveImage(handler->getOutputPath(), outputMatrix);
+
+        uintmax_t resultSize = filesystem::file_size(handler->getOutputPath(), ec);
+        if (ec) {
+            cerr << "Error getting output file size: " << ec.message() << endl;
+        } else {
+            cout << "Result image size: " << (resultSize / 1024.0) << " KB" << endl;
+        }
+
         cout << "Gif saved successfully: " << handler->getGifPath() << endl;
 
         auto end = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start); 
-
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
         cout << "Processing time: " << duration.count() << " ms" << endl;
     }
+
     delete handler;
     delete blok;
 
     return 0;
-
 }
